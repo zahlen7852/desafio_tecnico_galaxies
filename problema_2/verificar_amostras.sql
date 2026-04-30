4ed532ca53f5584d83f45226e5391766383cddb3
zahlen7852 <zahlen7852@gmail.com>
2026-04-29 21:46:20 -0300

    Delete problema_2/verificar_amostras.sql

--- problema_2/verificar_amostras.sql
+++
@@ -1,7 +0,0 @@
--- 5 linhas de cada tabela (apos a carga no Postgres)
-SELECT * FROM generation ORDER BY id LIMIT 5;
-SELECT * FROM pokemon ORDER BY id LIMIT 5;
-SELECT * FROM ability ORDER BY id LIMIT 5;
-SELECT * FROM move ORDER BY id LIMIT 5;
-SELECT * FROM pokemon_ability ORDER BY pokemon_id, ability_id LIMIT 5;
-SELECT * FROM pokemon_move ORDER BY pokemon_id, move_id LIMIT 5;
